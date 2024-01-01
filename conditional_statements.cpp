89de82ee02967eaa5f1055e6edd0c7d40acb1983
SWESH1K <136169450+SWESH1K@users.noreply.github.com>
2024-01-01 18:40:35 +0530

    first commit

---
+++ conditional_statements.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    cout<<"Please enter the a: ";
+    cin>>a;
+    cout<<"Please enter the b: ";
+    cin>>b;
+    cout<<"Their sum = "<<a+b;
+}
---
+++ hello_world.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a=4,b=65;
+    cout<<a+b;
+}
